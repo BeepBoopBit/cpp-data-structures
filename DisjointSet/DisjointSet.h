8824c0cdef9b42e80596e29b3fbb2a521714fe0a
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-05-15 10:46:31 +0800

    Renamed UnionFind -> DisjointSet

---
+++ DisjointSet/DisjointSet.h
@@ -0,0 +1,25 @@
+#ifndef UNION_FIND_H
+#define UNION_FIND_H
+
+// MakeSet
+// Find
+// union
+
+class DisjointSet{
+
+private:
+
+public:
+
+    
+
+public:
+
+
+private:
+
+
+};
+
+
+#endif // UNION_FIND_H
